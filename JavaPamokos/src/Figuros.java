c357348cbfa007d50154cf1fd37430565b790748
DESKTOP-1TDGMUF\Nemesis <linaskamin@gmail.com>
2018-11-14 12:09:00 +0200

    add figuros

--- JavaPamokos/src/Figuros.java
+++ JavaPamokos/src/Figuros.java
@@ -14,6 +14,12 @@ public class Figuros {
         p = P / 2;
         S = Math.sqrt(p* (p - a)*(p - b)*(p - c));
         System.out.println(S);
+        if (S%2 == 0 ) {
+            System.out.println("Skaicius lyginis");
+        } else {
+            System.out.println("Skaicius nelyginis");
+        }
+
 
         int Sst, ast, bst;
         System.out.println("Iveskite staciakampio krastines ilgi: ");
@@ -21,12 +27,26 @@ public class Figuros {
         bst = 8;
         Sst = ast*bst;
         System.out.println(Sst);
+        if (Sst%2 == 0 ) {
+            System.out.println("Skaicius lyginis");
+        } else {
+            System.out.println("Skaicius nelyginis");
+        }
+
 
         double Saps, Raps;
         System.out.println("Iveskite apskritimo spinduli: ");
         Raps = area.nextInt();
         Saps = Math.PI*(Raps * Raps);
         System.out.println(Saps);
+        if (Saps%2 == 0) {
+            System.out.println("Skaicius lyginis");
+        } else {
+            System.out.println("Skaicius nelyginis");
+        }
+
+
+
 
     }
 
