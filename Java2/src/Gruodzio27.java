a9fe6647f37519fb3a11a64a1a904c79eb260f53
DESKTOP-1TDGMUF\Nemesis <linaskamin@gmail.com>
2019-01-07 14:05:23 +0200

    ObjectStreams

---
+++ Java2/src/Gruodzio27.java
@@ -0,0 +1,18 @@
+package Java2.src;
+
+import java.awt.*;
+
+public class Gruodzio27 {
+    public static void main(String[] args) {
+        //enum
+        //enum kaip klase tik turi pavadinimus konstante ir tai yra objektas
+
+        Color color = Color.WHITE;
+        //e.printStackTrace
+        //--------------------------------------
+        //sausio2
+
+        //srautai tai lipinamos funkcijos
+        //Duomenu strukturos: map stream stack...
+    }
+}
---
+++ Java2/src/ObjectStreams.java
@@ -0,0 +1,63 @@
+//package Java2.src;
+
+public class ObjectStreams {
+    public static void main(String[] args) {
+
+        D61Node d = new D61Node("d");
+        D61Node e = new D61Node("e");
+
+        D61Node b = new D61Node("b");
+        b.setChildL(d);
+        b.setChildR(e);
+
+        D61Node c = new D61Node("c");
+
+        D61Node a = new D61Node("a");
+        a.setChildL(b);
+        a.setChildR(c);
+
+        spausdinti(a);
+
+    }
+
+    public static void spausdinti(D61Node root) {
+        if (root  != null){
+            System.out.println(" " + root.name);
+            spausdinti(root.childR);
+            spausdinti(root.childL);
+        }
+    }
+
+    static class D61Node {
+        String name;
+        D61Node childL, childR;
+
+        public D61Node(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public D61Node getChildL() {
+            return childL;
+        }
+
+        public void setChildL(D61Node childL) {
+            this.childL = childL;
+        }
+
+        public D61Node getChildR() {
+            return childR;
+        }
+
+        public void setChildR(D61Node childR) {
+            this.childR = childR;
+        }
+    }
+}
